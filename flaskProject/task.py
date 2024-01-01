12edbf60c3e587707d2e86e6d03e94b520dc32d1
Weng JinPeng <Weng_Jin_Peng@outlook.com>
2024-01-01 18:29:24 +0700

    优化目录结构

--- flaskProject/task.py
+++
@@ -1,6 +0,0 @@
-from apscheduler.schedulers.background import BackgroundScheduler
-
-sched = BackgroundScheduler(daemon=True)
-
-sched.add_job(lambda : sched.print_jobs(),'interval',seconds=5)
-sched.start()
